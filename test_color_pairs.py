2bcb4c63bf047a8c34a9985ac4b60cac0888dfe8
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-09-24 13:38:53 +0530

    Update test_color_pairs.py

--- test_color_pairs.py
+++ test_color_pairs.py
@@ -1,22 +1,23 @@
-
 from color_pair import get_color_from_pair_number, get_pair_number_from_color
 from color_data import MAJOR_COLORS, MINOR_COLORS
 
 def test_number_to_pair():
+    """Tests number to color pair mapping."""
     for i in range(1, 26):
-        major_color, minor_color = get_color_from_pair_number(i)
-        assert (major_color, minor_color) == get_color_from_pair_number(i)
+        assert get_color_from_pair_number(i) == get_color_from_pair_number(i)
 
 def test_pair_to_number():
+    """Tests color pair to number mapping."""
     for major_color in MAJOR_COLORS:
         for minor_color in MINOR_COLORS:
             expected_pair_number = get_pair_number_from_color(major_color, minor_color)
             assert expected_pair_number == get_pair_number_from_color(major_color, minor_color)
 
 def run_tests():
+    """Runs all test cases."""
     test_number_to_pair()
     test_pair_to_number()
-    print('Done:)')
+    print('All tests passed!')
 
 if __name__ == '__main__':
     run_tests()
