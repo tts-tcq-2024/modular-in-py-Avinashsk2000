4ccdd8e98182cd1e069fae950218601b2bafedd8
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-09-27 07:47:39 +0530

    Update test_color_pairs.py

--- test_color_pairs.py
+++ test_color_pairs.py
@@ -1,17 +1,14 @@
 from color_pair import get_color_from_pair_number, get_pair_number_from_color, print_reference_manual
 from color_data import MAJOR_COLORS, MINOR_COLORS
 import io, sys
-
 def test_number_to_pair():
     for pair_number in range(1, 26):
         assert get_color_from_pair_number(pair_number) == get_color_from_pair_number(pair_number)
-
 def test_pair_to_number():
     for major_color in MAJOR_COLORS:
         for minor_color in MINOR_COLORS:
             assert get_pair_number_from_color(major_color, minor_color) == \
                    get_pair_number_from_color(major_color, minor_color)
-
 def test_reference_manual_output():
     captured_output = io.StringIO()
     sys.stdout = captured_output
@@ -20,12 +17,10 @@ def test_reference_manual_output():
     expected_output = '\n'.join([f'{pair_number}: {get_color_from_pair_number(pair_number)[0]} {get_color_from_pair_number(pair_number)[1]}' 
                                  for pair_number in range(1, 26)]) + '\n'
     assert captured_output.getvalue() == expected_output
-
 def run_tests():
     test_number_to_pair()
     test_pair_to_number()
     test_reference_manual_output()
     print("Done:)")
-
 if __name__ == '__main__':
     run_tests()
